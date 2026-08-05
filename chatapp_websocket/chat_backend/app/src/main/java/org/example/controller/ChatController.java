87f55cad03d946669b84da8693276502ba1948e3
ma3llim <mohdsameer68257@gmail.com>
2026-08-05 20:39:27 +0530

    feat: update CORS configuration for Chat and Room controllers; change allowed origin to localhost:5173

--- chatapp_websocket/chat_backend/app/src/main/java/org/example/controller/ChatController.java
+++ chatapp_websocket/chat_backend/app/src/main/java/org/example/controller/ChatController.java
@@ -8,10 +8,12 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
+@CrossOrigin("http://localhost:5173")
 public class ChatController {
     private final RoomRepository roomRepository;
 
--- chatapp_websocket/chat_backend/app/src/main/java/org/example/controller/RoomController.java
+++ chatapp_websocket/chat_backend/app/src/main/java/org/example/controller/RoomController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
+@CrossOrigin("http://localhost:5173")
 public class RoomController {
     private final RoomService roomService;
 
