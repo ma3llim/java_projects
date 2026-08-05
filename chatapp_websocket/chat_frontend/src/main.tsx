21b76d8f099db8d1ddf494c30b42c80f6eb3f4bf
ma3llim <mohdsameer68257@gmail.com>
2026-08-05 19:44:50 +0530

    feat: added the join or create room UI

--- chatapp_websocket/chat_frontend/src/main.tsx
+++ chatapp_websocket/chat_frontend/src/main.tsx
@@ -1,15 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { ToastContainer } from "react-toastify";
+import JoinCreateChat from "./components/JoinCreateChat";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route index path="/" element={<App />} />
+                <Route index path="/" element={<JoinCreateChat />} />
             </Routes>
         </BrowserRouter>
+        <ToastContainer />
     </StrictMode>,
 );
